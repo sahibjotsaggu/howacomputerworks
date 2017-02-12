3933aaefc8d6e8ffbc586e7c6162d163bad4e6da
Sahibjot Saggu <sahibjot@live.ca>
2017-02-11 23:25:55 -0500

    change favicon

---
+++ authEnv.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'AUTH0_DOMAIN': 'scouter.auth0.com',
+	'AUTH0_CLIENT_ID': 'qmfEjOTIZcaw8Pp3QX5IWydyNbWmEB9H',
+	'AUTH0_CLIENT_SECRET':'EX12E8cUbyiKLm4h2Ee8hSdsLugw5BLLkg6eqNmKeBMy5VqZEBHbiLG_HDgK0qLf'
+};
